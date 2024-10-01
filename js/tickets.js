acfa7bbd92f58f95038c3dda0195351b2156efab
madsfjeldberg <mafj0001@stud.kea.dk>
2024-10-01 16:05:15 +0200

    initial framwork for getshowings, getbookings done. adding comments next.

--- js/tickets.js
+++ js/tickets.js
@@ -1,8 +1,12 @@
 let showingList = ["movie1", "movie2", "movie3"]
 
+const getShowingsTest = list => {
+    return showingList
+}
+
 const getShowings = async () => {
     try {
-        const res = await fetch(`http://localhost/allshowings`);
+        const res = await fetch(`http://localhost:8080/api/v1/allshowings`);
         if (!res.ok) {
             throw new Error('Network response was not ok');
         }
@@ -10,6 +14,52 @@ const getShowings = async () => {
         console.log(showings);
         return showings;
     } catch (error) {
-        
+        console.error('Problem with fetch operation on getShowings: ', error);
+    }
+}
+
+// takes a json object of all showings, and converts it to a list of objects
+const formFormatter = json => {
+
+    let showingList = [];
+
+    for (let showing of json) {
+        let theatre = showing.theatre;
+        let movie = showing.movie.title;
+        let time = showing.startTime;    
+        showingList.push({
+            theatre: theatre,
+            movie: movie,
+            time: time
+        })
+    }
+
+    // convert to html for use in form control
+    let html = "";
+    for (let showing of showingList) {
+        html += `<option value="${showing.theatre}">${showing.movie} - ${showing.time}</option>`
+    }
+
+    return html;
+}
+
+addEventListener('DOMContentLoaded', async () => {
+    let showings = await getShowings();
+    let html = formFormatter(showings);
+    document.getElementById('selectBody').innerHTML = html;
+})
+
+
+const getBookings = async showingId => {
+    try {
+        const res = await fetch(`http://localhost/api/v1/bookings?showingId=${showingId}`);
+        if (!res.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const showings = await res.json();
+        console.log(showings);
+        return showings;
+    } catch (error) {
+        console.error('Problem with fetch operation on getbookings: ', error);
     }
 }
