c92cd9f8788f6012e99f173903074c38c310136c
Chris <chye0001@stud.kea.dk>
2024-10-02 17:06:41 +0200

    used extract method for refactoring

--- js/showings.js
+++ js/showings.js
@@ -1,40 +1,72 @@
 const SHOWINGS_URL = "http://127.0.0.1:8080/api/v1/showings"
 
+
+
+
+
 async function displayShowings(movieTitle) {
     try {
         const response = await fetch(SHOWINGS_URL + "?title=" + movieTitle);
         checkForErrors(response);
-        const showingsList = await response.json();
+        const showingList = await response.json();
 
         const showingsGrid = document.getElementById("showings-grid");
         const nextSevenDaysFromCurrentDate = getNextSevenDays();
 
         for (const showingDay of nextSevenDaysFromCurrentDate) {
-            const column = document.createElement("div");
-            column.classList.add("col");
+            let column = buildColumn(showingDay, showingList);
+            showingsGrid.appendChild(column);
+        }
 
-            const header = document.createElement("div");
-            header.classList.add("column-header", "text-center");
+    } catch (error) {
+        console.error(error.message);
+    }
+}
 
-            const dateOptions = {weekday: 'short', month: 'short', day: 'numeric'};
-            header.innerText = showingDay.toLocaleDateString('en-US', dateOptions); // Show full date as a header
-            column.appendChild(header);
 
-            const filteredShowings = showingsList.filter(showing => {
-                const showingDate = parseJsonLocalDateTimeToDate(showing.startTime.match(/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/));
-                return showingDay.toDateString() === showingDate.toDateString();
-            });
 
-            filteredShowings.forEach(showing => {
-                const showingCard = document.createElement("a");
-                showingCard.classList.add("text-decoration-none");
 
-                const showingDate = parseJsonLocalDateTimeToDate(showing.startTime.match(/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/));
 
-                const timeOptions = { hour: '2-digit', minute: '2-digit', hour12: false };
-                const showingTime = showingDate.toLocaleTimeString('default', timeOptions);
 
-                showingCard.innerHTML += `
+function buildColumn(showingDay, showingList) {
+    const column = document.createElement("div");
+    column.classList.add("col");
+
+    const header = document.createElement("div");
+    header.classList.add("column-header", "text-center");
+
+    const dateOptions = {weekday: 'short', month: 'short', day: 'numeric'};
+    header.innerText = showingDay.toLocaleDateString('en-US', dateOptions); // Show full date as a header
+    column.appendChild(header);
+
+    const filteredShowings = showingList.filter(showing => {
+        const showingDate = parseJsonLocalDateTimeToDate(showing.startTime.match(/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/));
+        return showingDay.toDateString() === showingDate.toDateString();
+    });
+
+    filteredShowings.forEach(showing => {
+        let showingCard = buildCard(showing);
+        column.appendChild(showingCard);
+    });
+
+    return column;
+}
+
+
+
+
+
+
+function buildCard(showing) {
+    const showingCard = document.createElement("a");
+    showingCard.classList.add("text-decoration-none");
+
+    const showingDate = parseJsonLocalDateTimeToDate(showing.startTime.match(/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/));
+
+    const timeOptions = {hour: '2-digit', minute: '2-digit', hour12: false};
+    const showingTime = showingDate.toLocaleTimeString('default', timeOptions);
+
+    showingCard.innerHTML += `
                     <div class="card showing-card">
                         <div class="card-body">
                             <h7 class="card-title">${showing.theatre.name}</h7>
@@ -43,17 +75,13 @@ async function displayShowings(movieTitle) {
                     </div>
                 `;
 
-                showingCard.addEventListener("click", displaySeatBooking)
-                column.appendChild(showingCard);
-            });
-
-            showingsGrid.appendChild(column);
-        }
-
-    } catch (error) {
-        console.error(error.message);
-    }
+    showingCard.addEventListener("click", displaySeatBooking
+    return showingCard;
 }
+
+
+
+
 
 
 function checkForErrors(response) {
@@ -64,6 +92,10 @@ function checkForErrors(response) {
         throw error;
     }
 }
+
+
+
+
 
 
 function getNextSevenDays() {
@@ -78,6 +110,11 @@ function getNextSevenDays() {
 
     return daysArray;
 }
+
+
+
+
+
 
 function parseJsonLocalDateTimeToDate(jsonLocalDateTime) {
     if (!jsonLocalDateTime) {
@@ -94,6 +131,11 @@ function parseJsonLocalDateTimeToDate(jsonLocalDateTime) {
 
     return new Date(year, month, day, hours, minutes, seconds);
 }
+
+
+
+
+
 
 function displaySeatBooking() {
     alert("redirect to seatbooking")
