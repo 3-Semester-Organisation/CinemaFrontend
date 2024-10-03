d7ff1edc56001b4b921eb52449d8a76b7924ab9d
Lars <144362461+laha0006@users.noreply.github.com>
2024-10-03 12:20:24 +0200

    changes

--- js/bookSeat.js
+++ js/bookSeat.js
@@ -1,3 +1,25 @@
 export function initSeatView() {
     console.log("Init seat view!")
+    console.log(calcSeatViewWidth(15));
+}
+
+const seatView = document.querySelector("#seatView");
+const seatWidth = 20;
+
+function calcSeatViewWidth(seatNumber) {
+    let margin = 8 // mx-1
+    return (seatWidth+margin) * seatNumber + 80;
+}
+
+function createScreen() {
+    const screen = document.createElement("div");
+    screen.id = "screen"
+    screen.classList.add("text-center mt-2")
+    return screen;
+}
+
+function createSeatView() {
+    const fragment = document.createDocumentFragment();
+    fragment.appendChild(createScreen());
+
 }
