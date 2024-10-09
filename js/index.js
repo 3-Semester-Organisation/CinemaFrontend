15e2f37e3e7c7bed9a8c59866cae9bd924f2e457
madsfjeldberg <mafj0001@stud.kea.dk>
2024-10-09 10:29:51 +0200

    updated search field and button to be prettier

--- js/index.js
+++ js/index.js
@@ -6,6 +6,22 @@ document.addEventListener("DOMContentLoaded", initApp);
 
 function initApp() {
   initializeViewNavigation();
-  const search = document.getElementById("movie-search-btn")
+  const search = document.getElementById("search-icon")
   search.addEventListener("click", getMovieSearchFilter)
 }
+
+
+// press enter for search
+document.addEventListener('DOMContentLoaded', (event) => {
+  const searchInput = document.getElementById('movie-search');
+  const searchButton = document.getElementById('search-icon');
+
+  if (searchInput && searchButton) {
+      searchInput.addEventListener('keypress', function(event) {
+          if (event.key === 'Enter') {
+              event.preventDefault(); // Prevent the default form submission
+              searchButton.click(); // Trigger the search button click
+          }
+      });
+  }
+});
