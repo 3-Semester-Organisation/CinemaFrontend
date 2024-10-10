e586186451e9af93bf85e8043e767d13d8d6b545
Dosei-desu <krispauly@hotmail.com>
2024-10-10 22:37:20 +0200

    cleaned up index.js

--- js/index.js
+++ js/index.js
@@ -1,37 +1,22 @@
-import { initializeViewNavigation } from "./router.js";
-import { getMovieSearchFilter } from "./movies.js";
-import {searchInputSuggestion} from "./search";
+import {initializeViewNavigation} from "./router.js";
+import {getMovieSearchFilter} from "./movies.js";
 
 document.addEventListener("DOMContentLoaded", initApp);
 
 
 function initApp() {
-  initializeViewNavigation();
+    initializeViewNavigation();
 
-  //TODO ONE OF THESE IS REDUNDANT (this one)
-  const search = document.getElementById("search-icon")
-  search.addEventListener("click", getMovieSearchFilter)
-  searchInput.addEventListener("keypress", (event) => {
-    if(event.key === "Enter"){
-      search.click();
-    }
-  })
+    //TODO ONE OF THESE IS REDUNDANT (this one)
+    const searchInput = document.getElementById('movie-search');
+    const searchButton = document.getElementById('search-icon');
+    const search = document.getElementById("search-icon")
+    search.addEventListener("click", getMovieSearchFilter)
+    searchInput.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault(); // Prevent the default form submission
+            search.click();
+            searchButton.click();
+        }
+    })
 }
-
-
-// TODO ONE OF THESE IS REDUNDANT
-// press enter for search
-document.addEventListener('DOMContentLoaded', (event) => {
-  const searchInput = document.getElementById('movie-search');
-  const searchButton = document.getElementById('search-icon');
-
-  if (searchInput && searchButton) {
-      searchInput.addEventListener('keypress', function(event) {
-          searc
-          if (event.key === 'Enter') {
-              event.preventDefault(); // Prevent the default form submission
-              searchButton.click(); // Trigger the search button click
-          }
-      });
-  }
-});
