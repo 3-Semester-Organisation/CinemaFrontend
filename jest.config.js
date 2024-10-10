5f2e6a73c4dc20651aca8c4f78157092e31093f4
madsfjeldberg <mafj0001@stud.kea.dk>
2024-10-10 14:53:16 +0200

    added jest for testing, made some tests for movies.js

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  roots: ['<rootDir>/'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Uncomment if you have a setup file
+};
